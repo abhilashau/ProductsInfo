33846c4a66bb9e06b5d61a3210c100377eee5813
abhilashau <50186380+abhilashau@users.noreply.github.com>
2019-05-02 00:21:20 +0100

    Add files via upload

---
+++ ColorsObj.java
@@ -0,0 +1,31 @@
+package ProductsPkg;
+
+public class ColorsObj {
+	
+	String color;
+	String rbgColor;
+	String skuid;
+	
+	public ColorsObj() {
+		super();
+	}
+	public String getColor() {
+		return color;
+	}
+	public void setColor(String color) {
+		this.color = color;
+	}
+	public String getRbgColor() {
+		return rbgColor;
+	}
+	public void setRbgColor(String rbgColor) {
+		this.rbgColor = rbgColor;
+	}
+	public String getSkuid() {
+		return skuid;
+	}
+	public void setSkuid(String skuid) {
+		this.skuid = skuid;
+	}
+	
+}
